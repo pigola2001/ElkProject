f91ffae865973b57d02345a2867789c35b079e64
pigola2001 <94646542+pigola2001@users.noreply.github.com>
2024-02-12 10:14:31 +0100

    Update print_own_proc.py

--- print_own_proc.py
+++ print_own_proc.py
@@ -6,6 +6,8 @@ from elkpy import sushierrors
 
 sushi = sc.SushiController('192.168.0.249:51051', sushi_proto_def='C:/Users/pietr/OneDrive/Desktop/Uni/Magistrale/Research_Project/sushi-gui/venv/Lib/site-packages/sushi_rpc.proto')
 
-buddy_id=sushi.audio_graph.get_track_id('own')
+#get the own track id
+own_id=sushi.audio_graph.get_track_id('own')
 
-print(sushi.audio_graph.get_track_processors(buddy_id))
+#print the processors on the own track
+print(sushi.audio_graph.get_track_processors(own_id))
